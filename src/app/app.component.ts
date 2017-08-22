784a26534c12b881c7d7e49d8c9458c0fd798188
franciszver <franciszver@outlook.com>
2017-08-22 00:11:48 -0700

    finished this page - https://angular.io/tutorial/toh-pt1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+  <label>name: </label>
+  <input [(ngModel)]="hero.name" placeholder="name">
+  </div>`,
+  // templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
