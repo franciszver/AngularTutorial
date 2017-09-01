b4c241456798cbd9c12cd5033786b8ba76fb792c
Francisco DeGuzman <u031983@Global.Bio-Rad.com>
2017-09-01 16:35:17 -0700

    finished routing and styling https://angular.io/tutorial/toh-pt5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
-import { HeroService } from './hero.service';
-import { HeroDetailComponent } from './hero-detail.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [HeroService],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
